a1ce012a00c2e071d9a96f3f4d84f22f2b6e2e10
cricketworld234 <smart2020world@gmail.com>
2022-04-08 15:10:41 +0530

    Updates

---
+++ precache-manifest.3cf017dcac3ba96a69a602d858317b7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c22cec4e6f0ef738e60f",
+    "url": "/mera2nd-app/static/css/main.ce689a72.chunk.css"
+  },
+  {
+    "revision": "c22cec4e6f0ef738e60f",
+    "url": "/mera2nd-app/static/js/main.c22cec4e.chunk.js"
+  },
+  {
+    "revision": "38aaeeb32a2a2642a9a8",
+    "url": "/mera2nd-app/static/css/1.f4d00c7b.chunk.css"
+  },
+  {
+    "revision": "38aaeeb32a2a2642a9a8",
+    "url": "/mera2nd-app/static/js/1.38aaeeb3.chunk.js"
+  },
+  {
+    "revision": "4ed068930a1bbff8870d",
+    "url": "/mera2nd-app/static/js/runtime~main.4ed06893.js"
+  },
+  {
+    "revision": "a137993bbd09d98cefa0f4d836da5024",
+    "url": "/mera2nd-app/static/media/p1.a137993b.jpg"
+  },
+  {
+    "revision": "cda343b190efcdb0b8b819c41952ba6a",
+    "url": "/mera2nd-app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/mera2nd-app/precache-manifest.3cf017dcac3ba96a69a602d858317b7f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/mera2nd-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
